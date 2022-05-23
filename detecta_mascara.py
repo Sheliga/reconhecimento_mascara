bc21f29a2b4ad1fdee560c36887ef68a1f0a2f1b
Paulo Sheliga <paulosheliga@gmail.com>
2022-05-23 14:03:06 -0300

    feat: definição das tasks

--- detecta_mascara.py
+++ detecta_mascara.py
@@ -1,4 +1,18 @@
+import cv2 as cv
+
+
+def carregar_imagem():
+    imagem = cv.imread("conhecido/img1.jpg")
+    return imagem
+
+
 def main():
+    frame = carregar_imagem()
+    #Imagem de saida
+    cv.imshow("Frame", frame)
+    cv.waitKey(0) 
+ 
     print("print")
 
-main()
+if __name__ == '__main__':
+    main()
